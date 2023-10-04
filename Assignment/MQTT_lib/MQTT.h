c05a9cc20624bb37a90177159ef954eb6fdec5aa
evanic14 <emiel.visser12@gmail.com>
2023-10-04 21:41:08 +0200

    More MQTT stuff. Final?

--- Assignment/MQTT_lib/MQTT.h
+++ Assignment/MQTT_lib/MQTT.h
@@ -3,37 +3,17 @@
 
 #include <mqtt/async_client.h>
 #include <string>
+#include <memory>
 
-class mqtt {
-	mqtt(onst std::string& brokerAddress, const std::string& clientId)
-        : brokerAddress_(brokerAddress), clientId_(clientId) {
-        client_ = std::make_unique<mqtt::async_client>(brokerAddress, clientId);
-    }
-
-    ~MqttPublisher() {
-        disconnect();
-    }
-
-    void connect() {
-        mqtt::connect_options conn_opts;
-        conn_opts.set_keep_alive_interval(20);
-        conn_opts.set_clean_session(true);
-
-        client_->connect(conn_opts);
-    }
-
-    void publish(const std::string& topic, const std::string& message, int qos = 0) {
-        mqtt::message_ptr msg = mqtt::make_message(topic, message);
-        msg->set_qos(qos);
-
-        client_->publish(msg)->wait_for(5000);
-    }
-
-    void disconnect() {
-        if (client_->is_connected()) {
-            client_->disconnect();
-        }
-    }
+class MqttPublisher {
+public:
+	MqttPublisher(const std::string& brokerAddress, const std::string& clientId);
+    ~MqttPublisher();
+    
+    void connect();
+    void publish(const std::string& topic, const std::string& message, int qos = 0);
+    void disconnect();
+       
 
 private:
     std::string brokerAddress_;
@@ -41,5 +21,5 @@ private:
     std::unique_ptr<mqtt::async_client> client_;
 };
 
-#endif  // MQTT_PUBLISHER_HPP
-}
+#endif
+
